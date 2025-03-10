d57e2075c38152ee0b271521076c988e4fa402f7
Andy8787(布長) <andy8787main@gmail.com>
2025-03-10 21:26:27 +0800

    更改ut.cpp

--- Unit_test/fms.cpp
+++ Unit_test/fms.cpp
@@ -1,69 +1,146 @@
-#include <stdio.h>
-// fms.cpp
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef enum {
-    start,
-    build_id,
-    build_num,
-    build_invalid,
-    identifier,
-    number,
-    invalid,
-    stop
-} state_t;
-state_t getNextState(state_t current_state, char ch) {
-    if (current_state == start) {
-        if (ch == ' ')
-            return start;
-        else if (isalpha(ch))
-            return build_id;
-        else if (isdigit(ch))
-            return build_num;
-        else if (ch == '#')
-            return stop;
+
+// 檢查特殊的case
+int is_special_case(char judge_str[]) {
+    int len = strlen(judge_str);
+    int is_special = 0;
+
+    if (judge_str[0] == '.') {
+        is_special = 1;
     }
-    if (current_state == build_id) {
-        if (isalpha(ch) || isdigit(ch) || (ch == '_'))
-            return build_id;
-        else if (ch == ' ')
-            return identifier;
+    if (judge_str[0] == '0') {
+        if (len == 2) {
+            is_special = 1;
+        }
+        if (len >= 3 && judge_str[1] != '.') {
+            is_special = 1;
+        }
     }
-    if (current_state == build_num) {
-        if (isdigit(ch))
-            return build_num;
-        else if (ch == ' ')
-            return number;
+    if (judge_str[0] == '-' && strcmp("-0", judge_str) == 0) {
+        is_special = 1;
     }
-    return stop;
+    return is_special;
 }
+
+// 判斷字串的類型
+char *judge(char judge_str[]) {
+    int len = strlen(judge_str);
+    int start_index = 0;
+
+    char out[1000];
+    char *output = (char *)malloc(sizeof(char) * 100000);
+
+    // 檢查特殊的case
+    if (judge_str[0] == '0') {
+        if (len == 1) {
+            sprintf(out, " - Positive Integer\n");
+            strcat(output, out);
+            return output;
+        }
+    }
+
+    int is_special = is_special_case(judge_str);
+    if (is_special == 1) {
+        sprintf(out, " - Error\n");
+        strcat(output, out);
+        return output;
+    }
+
+    // 狀態機
+    int float_status = 0;
+    int variable_status = 0;
+    int number_status = 0;
+    int negative_status = 0;
+
+    // 跳過負數的符號
+    if (judge_str[0] == '-') {
+        start_index = 1;
+        negative_status = 1;
+    }
+
+    // 檢查類型
+    for (int i = start_index; i < len; i++) {
+        char Key = judge_str[i];
+        if (Key == '.') {
+            if (float_status == 1 || variable_status == 1 || i == len - 1) {
+                sprintf(out, " - Error\n");
+                strcat(output, out);
+                return output;
+            }
+            float_status = 1;
+        } else if (Key >= '0' && Key <= '9') {
+            if (number_status == 0) {
+                number_status = 1;
+            }
+        } else if ((Key >= 'A' && Key <= 'Z') || (Key == '_')) {
+            if (number_status == 1 && variable_status == 0) {
+                sprintf(out, " - Error\n");
+                strcat(output, out);
+                return output;
+            }
+            variable_status = 1;
+        } else if ((Key <= 'z')) {
+            if (Key >= 'a') {
+                if (number_status == 1 && variable_status == 0) {
+                    sprintf(out, " - Error\n");
+                    strcat(output, out);
+                    return output;
+                }
+                variable_status = 1;
+            }
+        } else {
+            sprintf(out, " - Error\n");
+            strcat(output, out);
+            return output;
+        }
+    }
+
+    // 輸出類型
+    if (variable_status == 1) {
+        sprintf(out, " - Identifier\n");
+        strcat(output, out);
+    } else if (float_status == 1) {
+        if (negative_status == 1) {
+            sprintf(out, " - Negative Floating\n");
+            strcat(output, out);
+        } else {
+            sprintf(out, " - Positive Floating\n");
+            strcat(output, out);
+        }
+    } else {
+        // number_status == 1
+        if (negative_status == 1) {
+            sprintf(out, " - Negative Integer\n");
+            strcat(output, out);
+        } else {
+            sprintf(out, " - Positive Integer\n");
+            strcat(output, out);
+        }
+    }
+    return output;
+}
+
 char *FMS(char *s) {
-    // fms.cpp
-    char out[80];
-    char *output = (char *)malloc(sizeof(char) * 500);
-    char input_char;
-    int index = 0;
-    state_t current_state;
-    current_state = start;
-    strcpy(output, "");
-    do {
-        if (current_state == identifier) {
-            current_state = start;
-            sprintf(out, " - identifier\n");
-            strcat(output, out);
-        } else if (current_state == number) {
-            current_state = start;
-            sprintf(out, " - number\n");
-            strcat(output, out);
+    char out[1000];
+    char *output = (char *)malloc(sizeof(char) * 100000);
+
+    char judge_str[100];
+    int now_temp_index = 0;
+    for (int index = 0; index < strlen(s); index++) {
+        if (s[index] == ' ') {
+            strcat(output, judge_str);
+            strcat(output, judge(judge_str));
+            memset(judge_str, '\0', sizeof(judge_str));
+            now_temp_index = 0;
+        } else {
+            judge_str[now_temp_index] = s[index];
+            now_temp_index++;
+            judge_str[now_temp_index] = '\0';
         }
-        // scanf("%c", &input_char);
-        input_char = s[index++];
-        if ((input_char != ' ') && (input_char != '#')) {
-            sprintf(out, "%c", input_char);
-            strcat(output, out);
-        }
-        current_state = getNextState(current_state, input_char);
-    } while (current_state != stop);
+    }
+
     return output;
 }
--- Unit_test/ut.cpp
+++ Unit_test/ut.cpp
@@ -1,42 +1,50 @@
 // ut.cpp
 #include <gtest/gtest.h>
-
 #include <cstdlib>
-
 #include "fms.h"
+
 TEST(fmsTest, HandleInput) {
-    char s[] = "rate R2D2 -2 time 555666 0.23 -1.2 5a .123 0 01 0123 -0 12345... Abc... 12345. _hello_ $123$ AAA ___ a z A Z ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz _ 12345ABC {} #";
-    char expOutput[] =
-        "rate - Identifier\n"
-        "R2D2 - Identifier\n"
-        "-2 - Negative Integer\n"
-        "time - Identifier\n"
-        "555666 - Positive Integer\n"
-        "0.23 - Positive Floating\n"
-        "-1.2 - Negative Floating\n"
-        "5a - Error\n"
-        ".123 - Error\n"
-        "0 - Positive Integer\n"
-        "01 - Error\n"
-        "0123 - Error\n"
-        "-0 - Error\n"
-        "12345... - Error\n"
-        "Abc... - Error\n"
-        "12345. - Error\n"
-        "_hello_ - Identifier\n"
-        "$123$ - Error\n"
-        "AAA - Identifier\n"
-        "___ - Identifier\n"
-        "a - Identifier\n"
-        "z - Identifier\n"
-        "A - Identifier\n"
-        "Z - Identifier\n"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz - Identifier\n"
-        "_ - Identifier\n"
-        "12345ABC - Error\n"
-        "{} - Error\n";
-    ASSERT_STREQ(expOutput, FMS(s));
+    struct TestCase {
+        const char* input;
+        const char* expectedOutput;
+    };
+
+    TestCase testCases[] = {
+        {"rate", "rate - Identifier"},
+        {"R2D2", "R2D2 - Identifier"},
+        {"-2", "-2 - Negative Integer"},
+        {"time", "time - Identifier"},
+        {"555666", "555666 - Positive Integer"},
+        {"0.23", "0.23 - Positive Floating"},
+        {"-1.2", "-1.2 - Negative Floating"},
+        {"5a", "5a - Error"},
+        {".123", ".123 - Error"},
+        {"0", "0 - Positive Integer"},
+        {"01", "01 - Error"},
+        {"0123", "0123 - Error"},
+        {"-0", "-0 - Error"},
+        {"12345...", "12345... - Error"},
+        {"Abc...", "Abc... - Error"},
+        {"12345.", "12345. - Error"},
+        {"_hello_", "_hello_ - Identifier"},
+        {"$123$", "$123$ - Error"},
+        {"AAA", "AAA - Identifier"},
+        {"___", "___ - Identifier"},
+        {"a", "a - Identifier"},
+        {"z", "z - Identifier"},
+        {"A", "A - Identifier"},
+        {"Z", "Z - Identifier"},
+        {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz - Identifier"},
+        {"_", "_ - Identifier"},
+        {"12345ABC", "12345ABC - Error"},
+        {"{}", "{} - Error"}
+    };
+
+    for (const auto& testCase : testCases) {
+        ASSERT_STREQ(testCase.expectedOutput, FMS(testCase.input));
+    }
 }
+
 int main(int argc, char **argv) {
     testing ::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
