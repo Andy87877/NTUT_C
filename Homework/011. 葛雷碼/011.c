207207ae17d86d8b59336bd73a84541cf1c2f84f
Andy8787(布長) <andy8787main@gmail.com>
2025-03-11 16:12:45 +0800

    011. 葛雷碼 排版

--- Homework/011. 葛雷碼/011.c
+++ Homework/011. 葛雷碼/011.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-void G(int n, int k){
-    if (n == 1){
+void G(int n, int k) {
+    if (n == 1) {
         printf("%d", k);
         return;
     }
-    if (k < (pow(2, n-1))){
+    if (k < (pow(2, n - 1))) {
         printf("0");
-        return G(n-1, k);
+        return G(n - 1, k);
     } else {
         printf("1");
-        return G(n-1, pow(2, n)-1-k);
+        return G(n - 1, pow(2, n) - 1 - k);
     }
 }
 
-void Judge(int n, int k){
+void Judge(int n, int k) {
     G(n, k);
     printf("\n");
 }
 
-int main(){
+int main() {
     int n, k;
-    while (1 == 1){
+    while (1 == 1) {
         scanf("%d", &n);
-        if (n == -1){
+        if (n == -1) {
             break;
         }
         scanf("%d", &k);
         Judge(n, k);
     }
-
 }
