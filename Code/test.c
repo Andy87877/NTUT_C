ba5c9f68634bd96af5f5d93209a7bc872d79b466
Andy8787(布長) <andy8787main@gmail.com>
2025-03-15 20:29:14 +0800

    AI寫笑話 測試

--- Code/test.c
+++ Code/test.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-int main(){
-    int a,b;
-    scanf("%d %d", &a, &b);
-    double Area = (a*b)/2.0;
-
-    printf("Triangle area:%.1f\n", Area);
+int main() {
+    printf("Why do programmers prefer dark mode?\n");
+    printf("Because light attracts bugs!\n");
+    return 0;
 }
