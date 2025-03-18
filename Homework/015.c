a5458f64aba81246a62f5f55ff3188587c44c31f
Andy8787(布長) <andy8787main@gmail.com>
2025-03-18 18:00:08 +0800

    week050x 的練習

--- Homework/015.c
+++ Homework/015.c
@@ -4,6 +4,37 @@
 m = (y1 - y2) / (x1 - x2)
 b = (x2 * y1 - x1 * y2) / (x2 - x1)
 分數表示格式：分子/分母，例如7/3
+Numerator/Denominator (upper/lower)
 */
 
 #include <stdio.h>
+
+int abs(int x){
+    if (x < 0) return -x;
+    return x;
+}
+
+void equation(int x1, int y1, int x2, int y2){
+    if (x1 == x2){ // 分母為0
+        return;
+    }
+
+    int m_upper = (y1 - y2);
+    int m_lower = (x1 - x2);
+
+    int b_upper
+    int b = (x2 * y1 - x1 * y2) / (x2 - x1);
+
+
+}
+
+int main(){
+    int times;
+    scanf("%d", &times);
+
+    for (int i = 0; i < times; i++){
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        equation(x1, y1, x2, y2);
+    }
+}
