98a8d3cc43c7b3ff0717928590b571233f540f3a
Andy8787(布長) <andy8787main@gmail.com>
2025-03-19 21:36:51 +0800

    14 更改變數名稱

--- Homework/014. 分數四則運算/014.c
+++ Homework/014. 分數四則運算/014.c
@@ -1,4 +1,4 @@
-/* upper: 分子、lower: 分母、 real_number: 真分數*/
+/* numerator: 分子、denominator: 分母、 real_number: 真分數*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,22 +15,22 @@ int gcd(int m, int n) {
     return gcd(n, m % n);
 }
 
-void Get_fraction(char* fraction, int* new_upper, int* new_lower, int* error) {
+void Get_fraction(char* fraction, int* new_numerator, int* new_denominator, int* error) {
     *error = 0;
     int len = strlen(fraction);
     int now_number = 0;
-    int upper = -1, lower = -1, real_number = -1; // 分子 分母 真分數
+    int numerator = -1, denominator = -1, real_number = -1; // 分子 分母 真分數
     int negative = 0;
     for (int i = 0; i < len; i++) {
         char now_char = fraction[i];
 
         if (now_char == '/' || now_char == '(' || now_char == ')') {
             if (now_char == '/') {
-                upper = now_number;
+                numerator = now_number;
             } else if (now_char == '(') {
                 real_number = now_number;
             } else {
-                lower = now_number;
+                denominator = now_number;
             }
             now_number = 0;
             continue;
@@ -43,121 +43,121 @@ void Get_fraction(char* fraction, int* new_upper, int* new_lower, int* error) {
         now_number *= 10;
         now_number += ((now_char) - '0');
     }
-    if (lower == -1) lower = now_number;
+    if (denominator == -1) denominator = now_number;
     if (real_number != -1) {
-        upper += real_number * lower;
+        numerator += real_number * denominator;
     }
-    if (negative) upper *= -1;
-    if (lower == 0) {
+    if (negative) numerator *= -1;
+    if (denominator == 0) {
         *error = 1;
     }
 
-    *new_upper = upper;
-    *new_lower = lower;
+    *new_numerator = numerator;
+    *new_denominator = denominator;
 }
 
-void Output_answer(int upper, int lower) {
+void Output_answer(int numerator, int denominator) {
     int real_number;
-    if (lower == 0) {
+    if (denominator == 0) {
         printf("Error\n");
         return;
     }
 
-    real_number = upper / lower;
-    upper -= real_number * lower;
+    real_number = numerator / denominator;
+    numerator -= real_number * denominator;
 
-    // printf("%d %d %d\n", real_number, upper, lower);
+    // printf("%d %d %d\n", real_number, numerator, denominator);
     if (real_number == 0) {
-        if (lower == 1 || lower == -1) {
-            printf("%d", upper);
+        if (denominator == 1 || denominator == -1) {
+            printf("%d", numerator);
         } else {
-            printf("%d/%d", upper, lower);
+            printf("%d/%d", numerator, denominator);
         }
     } else {
-        if (lower == 1 || lower == -1) {
+        if (denominator == 1 || denominator == -1) {
             printf("%d", real_number);
         } else {
-            printf("%d(%d/%d)", real_number, abs(upper), abs(lower));
+            printf("%d(%d/%d)", real_number, abs(numerator), abs(denominator));
         }
     }
     printf("\n");
 }
 
-void Compute(int upper_1, int lower_1, int upper_2, int lower_2,
+void Compute(int numerator_1, int denominator_1, int numerator_2, int denominator_2,
              char operater) {
-    int upper_answer = 0, lower_answer = 0;
+    int numerator_answer = 0, denominator_answer = 0;
 
     if (operater == '+' || operater == '-') { // 加法 減法
         // 通分
-        int mul_number = lower_1 * lower_2;
-        upper_1 *= lower_2;
-        upper_2 *= lower_1;
-        lower_answer =
-            mul_number; // lower_1 = mul_number; lower_2 = mul_number;
+        int mul_number = denominator_1 * denominator_2;
+        numerator_1 *= denominator_2;
+        numerator_2 *= denominator_1;
+        denominator_answer =
+            mul_number; // denominator_1 = mul_number; denominator_2 = mul_number;
 
         if (operater == '+') {
-            upper_answer = upper_1 + upper_2;
+            numerator_answer = numerator_1 + numerator_2;
         }
         if (operater == '-') {
-            upper_answer = upper_1 - upper_2;
+            numerator_answer = numerator_1 - numerator_2;
         }
     }
     if (operater == '*') {
-        upper_answer = upper_1 * upper_2;
-        lower_answer = lower_1 * lower_2;
+        numerator_answer = numerator_1 * numerator_2;
+        denominator_answer = denominator_1 * denominator_2;
     }
     if (operater == '/') {
-        upper_answer = upper_1 * lower_2;
-        lower_answer = lower_1 * upper_2;
+        numerator_answer = numerator_1 * denominator_2;
+        denominator_answer = denominator_1 * numerator_2;
     }
 
     int real_number = 0;
-    if (lower_answer == 0) {
+    if (denominator_answer == 0) {
         printf("Error\n");
         return;
     }
-    if (lower_answer == 0) {
+    if (denominator_answer == 0) {
         printf("Error\n");
         return;
     }
-    if (upper_answer == 0) {
+    if (numerator_answer == 0) {
         printf("0\n");
         return;
     }
-    // printf("Output1: %d, %d\n",upper_answer, lower_answer);
-    int answer_gcd = gcd(abs(upper_answer), abs(lower_answer));
+    // printf("Output1: %d, %d\n",numerator_answer, denominator_answer);
+    int answer_gcd = gcd(abs(numerator_answer), abs(denominator_answer));
     // printf("answer_gcd: %d\n", answer_gcd);
-    upper_answer /= answer_gcd;
-    lower_answer /= answer_gcd;
-    // printf("Output2: %d, %d\n",upper, lower);
+    numerator_answer /= answer_gcd;
+    denominator_answer /= answer_gcd;
+    // printf("Output2: %d, %d\n",numerator, denominator);
 
-    Output_answer(upper_answer, lower_answer);
+    Output_answer(numerator_answer, denominator_answer);
 }
 
 void Input_data() {
     char fraction_1[101], fraction_2[101], operater;
-    int upper_1, lower_1, upper_2, lower_2, error_1, error_2;
+    int numerator_1, denominator_1, numerator_2, denominator_2, error_1, error_2;
 
     fgets(fraction_1, 101, stdin);
     fraction_1[strlen(fraction_1) - 1] = '\0';
-    Get_fraction(fraction_1, &upper_1, &lower_1, &error_1);
-    // printf("%d / %d , error: %d\n", upper_1, lower_1, error_1);
+    Get_fraction(fraction_1, &numerator_1, &denominator_1, &error_1);
+    // printf("%d / %d , error: %d\n", numerator_1, denominator_1, error_1);
 
     scanf("%c", &operater);
     getchar();
 
     fgets(fraction_2, 101, stdin);
     fraction_2[strlen(fraction_2) - 1] = '\0';
-    Get_fraction(fraction_2, &upper_2, &lower_2, &error_2);
-    // printf("%d / %d , error: %d\n", upper_2, lower_2, error_2);
+    Get_fraction(fraction_2, &numerator_2, &denominator_2, &error_2);
+    // printf("%d / %d , error: %d\n", numerator_2, denominator_2, error_2);
 
     if (error_1 || error_2) {
         printf("Error\n");
         return;
     }
-    // printf("Compute: %d, %d, %d, %d, %c\n", upper_1, lower_1, upper_2,
-    // lower_2, operater);
-    Compute(upper_1, lower_1, upper_2, lower_2, operater);
+    // printf("Compute: %d, %d, %d, %d, %c\n", numerator_1, denominator_1, numerator_2,
+    // denominator_2, operater);
+    Compute(numerator_1, denominator_1, numerator_2, denominator_2, operater);
 }
 
 int main() {
