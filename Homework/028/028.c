542930c21ef3fec539edd631a790ad13d69b721d
Andy8787(布長) <andy8787main@gmail.com>
2025-05-05 14:39:04 +0800

    28題 暗測未過

---
+++ Homework/028/028.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 60
+
+// 檢查大寫或小寫
+int Check_big_or_small(char check_char) {
+    if (check_char >= 'A' && check_char <= 'Z') {
+        return 1; // 大寫
+    }
+    return 0; // 小寫
+}
+
+void Judge() {
+    char string[SIZE];
+    string[0] = '\0';
+    int k;
+
+    scanf("%s %d", &string, &k);
+
+    int big_times = 0, small_times = 0;
+    int times_array[SIZE], index = 0;
+    int is_big_check = 0;
+
+    for (int i = 0; i < strlen(string); i++) {
+        char now_char = string[i];
+        int is_big = Check_big_or_small(now_char);
+
+        if (is_big_check != is_big && i != 0) {
+            if (is_big_check == 1) {
+                times_array[index] = big_times;
+                big_times = 0;
+            } else {
+                times_array[index] = small_times;
+                small_times = 0;
+            }
+            index++;
+        }
+
+        if (is_big == 1) {
+            big_times++;
+            is_big_check = 1;
+        } else {
+            small_times++;
+            is_big_check = 0;
+        }
+    }
+    // last
+    if (is_big_check == 1) {
+        times_array[index] = big_times;
+        big_times = 0;
+    } else {
+        times_array[index] = small_times;
+        small_times = 0;
+    }
+    index++;
+
+    int size = index, answer = 0, tmp = 0;
+
+    for (int i = 0; i < size; i++) {
+        // printf("%d ", times_array[i]);
+
+        int zero_flag = 0;
+        if (times_array[i] == k) {
+            tmp++;
+        } else {
+            if (times_array[i] > k) {
+                tmp++;
+            }
+            if (tmp == 1 && times_array[i] > k) { // 最前面
+                zero_flag = 0;
+            } else {
+                zero_flag = 1;
+            }
+        }
+
+        if (answer < tmp && tmp != 1) {
+            answer = tmp;
+        }
+
+        if (zero_flag == 1) {
+            tmp = 0;
+        }
+    }
+    answer = answer * k;
+
+    printf("%d\n", answer);
+}
+
+int main() {
+    int times;
+    scanf("%d", &times);
+
+    for (int t = 0; t < times; t++) {
+        Judge();
+    }
+}
