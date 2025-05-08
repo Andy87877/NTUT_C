b82aef9a57ef9e286ba380b918facd30dbdb45f9
Andy8787(布長) <andy8787main@gmail.com>
2025-05-08 14:39:35 +0800

    29題 參考AI

--- Homework/029/029.c
+++ Homework/029/029.c
@@ -2,35 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define longd long double
+double f1(double x, double a) { return sqrt(a - x * x); }
 
-longd f1(longd x, longd a) {
-    longd num;
-    num = sqrt(a - pow(x, 2));
-    return num;
-}
-
-longd f2(longd x, longd a) {
-    longd up_num, down_num, num;
-
-    up_num = a * pow(x, 3) + 7 * x;
-    down_num = sqrt(a + x);
-    num = up_num / down_num;
-
-    return num;
+double f2(double x, double a) {
+    return ((a * pow(x, 3) + 7 * x) / (sqrt(a + x)));
 }
 
 void judge(int mode) {
     // 輸入四個參數a(函數中的係數)、p(積分區間起始點)、q(積分區間終點)、err(精度要求，表示精確到小數後第error位)
-    longd a, p, q, err;
-    scanf("%d %d %d %d", &a, &p, &q, &err);
+    int A, P, Q, err;
+    scanf("%d %d %d %d", &A, &P, &Q, &err);
 
-    longd T, h;
+    double a, p, q;
+    a = (double)A;
+    p = (double)P;
+    q = (double)Q;
 
-    int delta = 0;
-    while (1){
+    double times = 0;
+    int n = 1; // 切割次數
+    double T_now, T_old = -1;
 
+    while (1) {
+        double h, sum_num = 0;
+        h = (q - p) * 1.0 / n;
+
+        T_now = 0;
+
+        if (mode == 1) {
+            T_now = 1.0 * f1(p, a) + 1.0 * f1(q, a);
+            for (int i = 1; i < n; i++) {
+                T_now += (2.0 * f1((p + i * h), a));
+            }
+        } else {
+            T_now = 1.0 * f2(p, a) + 1.0 * f2(q, a);
+            for (int i = 1; i < n; i++) {
+                T_now += (2.0 * f2((p + i * h), a));
+            }
+        }
+        T_now = T_now * (h / 2);
+
+        // printf("%d, %.12f/%.12f\n", times, T_now, T_old);
+        // 表示精確到小數後第error位
+        if (abs(T_now - T_old) < pow(10, -err)) {
+            break;
+        }
+        n *= 2;
+        times++;
+
+        T_old = T_now;
     }
+
+    printf("%.12f\n", T_now);
 }
 
 int main() {
@@ -39,6 +61,10 @@ int main() {
     while (1) {
         scanf("%d", &mode);
         if (mode == 0) break;
+        if (mode != 1 && mode != 2) {
+            printf("Invalid\n");
+            continue;
+        }
 
         judge(mode);
     }
