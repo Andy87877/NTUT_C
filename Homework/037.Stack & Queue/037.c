d40134329c4e0a60622943ece1720a53e6eb80a3
Andy8787(布長) <andy8787main@gmail.com>
2025-06-01 20:36:51 +0800

    37 stack 剛開始寫

---
+++ Homework/037.Stack & Queue/037.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_s {
+        int data;
+        struct node_s* next;
+} node_t;
+
+typedef node_t* nodep_t;
+
+int main() {}
+
+/*
+stack 先進後出
+
+queue 先進先出
+
+
+*/
